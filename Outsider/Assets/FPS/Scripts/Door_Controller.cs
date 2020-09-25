dcb01481a14f7498a95ab08d5414055d1fa38b45
Z3ypher <29761853+Z3ypher@users.noreply.github.com>
2020-09-24 19:26:10 -0500

    Move affected doorto general
    Move affected doorto general

--- Outsider/Assets/FPS/Scripts/Door_Controller.cs
+++ Outsider/Assets/FPS/Scripts/Door_Controller.cs
@@ -9,17 +9,14 @@ public class Door_Controller : MonoBehaviour
 {	
 	public GameObject Door;
 	public bool doorIsOpening;
-	public Animator anim;
 	
 	
 	void Update() {  // moves the door up by 5 up in the y vector.
 		if (doorIsOpening == true){
 			Door.transform.Translate (Vector3.up * Time.deltaTime * 5);
-			anim.enabled = true;
-			anim.SetTrigger("up");
 		//if doorIsOpening is true, then open the door
 		}
-		if (Door.transform.position.y > 5f ){
+		if (Door.transform.position.y > 7f ){
 			doorIsOpening = false;
 		// If the door is already open, leave it alone.
 		}
