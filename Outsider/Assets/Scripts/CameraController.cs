dcdd461bf9a0d3a06a6768a196b7b11364f9e13d
Clucido <60236753+Clucido@users.noreply.github.com>
2020-09-28 16:23:34 -0500

    Basic Demo
    Basic stuff for demo, characters sprited, but not changed in functionality

---
+++ Outsider/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 cameraOffset;
+    public float smoothFollow;
+    void FollowTarget(){
+        Vector3 targetPos = target.position+cameraOffset;
+        transform.position = Vector3.Lerp(targetPos,transform.position,Time.deltaTime*smoothFollow);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget();
+    }
+}
