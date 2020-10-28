b1cd1df61f107f0c9142a23f608aabd31e723532
Clucido <60236753+Clucido@users.noreply.github.com>
2020-10-28 16:13:12 -0500

    Wednesday Update
    This is what collin has done so far on the last wednesday of sprint 1

---
+++ Outsider/Assets/Scripts/Object_Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveforce = 0f;
+    private Rigidbody rigidbody;
+    public GameObject projectile;
+    public Transform gun;
+    public float fireRate = 0f;
+    public float projectileForce = 0f;
+    private float fireRateTimeStamp = 0f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal")*moveforce;
+        float Vertical = Input.GetAxisRaw("Vertical")*moveforce;
+        rigidbody.velocity = new Vector3(Horizontal,Vertical,0);
+
+        if(Input.GetKey(KeyCode.E)){
+            if(Time.time > fireRateTimeStamp){
+                GameObject go = (GameObject)Instantiate(projectile,gun.position, gun.rotation);
+                go.GetComponent<Rigidbody>().AddForce(gun.forward * projectileForce);
+                fireRateTimeStamp = Time.time + fireRate;
+            }
+        }
+    }
+}
---
+++ Outsider/Assets/Scripts/Projectile_Collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Self;
+    public GameObject Destroy;
+    public GameObject SpawnOnCollide;
+    void OnCollisionEnter (Collision collisionInfo){
+        GameObject go = (GameObject)Instantiate(SpawnOnCollide,Self.position, Self.rotation);
+        Destroy(Destroy);
+    }
+
+}
